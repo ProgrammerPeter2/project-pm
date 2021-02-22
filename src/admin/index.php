a153cfedebb1ddbf0e89a0144485b9fddad618a1
ProgrammerPeter2 <66070387+ProgrammerPeter2@users.noreply.github.com>
2021-02-22 10:31:07 +0100

    delete
    the communication system removed.

--- src/admin/index.php
+++ src/admin/index.php
@@ -17,24 +17,23 @@
 			<th style="width: 12.25%">M&ucirc;veletek</th>
 		</tr>
 		<?php
-			include "../communication/encode.php";
 			include "../db_conn.php";
 			//mysql
 			$sql = "SELECT * FROM users";
 			$result = mysqli_query($conn, $sql);
 			while ($row = mysqli_fetch_array($result)) {
+				$_POST['un'] = $row['username'];
 				$AdminImage = '<img src="images/delete.png"/>';
 				if($row['admin'] == 1){
 					$AdminImage = '<img src="images/pipa.png"/>';
 				}
-				encode('uid',$row['id']);
 				echo "<tr>";
 				echo "<td> ".$row['id']." </td>";
 				echo "<td> ".$row['username']." </td>";
 				echo "<td>".$row['name']."</td>";
 				echo "<td>".$AdminImage."</td>";
 				echo "<td>";
-				echo '<form name="changePass" action="actions/chpass.php" method="POST"> <abbr title="Change user password"><input type="image" name="submit" src="images/change pass.png"/></abbr></form>';
+				echo '<form name="changePass" action="actions/chpassSite.php" method="POST"> <abbr title="Change user password"><input type="image" name="submit" src="images/change pass.png"/></abbr></form>';
 				echo '<form name="editUser" action="user_actions.php?act&uid=`e $uid`" method="POST"> <abbr title="Edit user datas"><input type="image" name="submit" src="images/edit.png" /></abbr></form>';
 				echo '<form name="deleteUser" action="user_actions.php?act&uid=`dlt $uid`" method="POST"> <abbr title="Delete user"><input type="image" name="submit" src="images/delete.png" /></abbr></form>';
 				echo "</td>";
